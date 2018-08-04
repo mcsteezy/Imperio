13f0afa3ea542058b523f2120d4e8291c6816c58
mcsteezy <davidpark9224@gmail.com>
2018-08-04 14:31:39 -0700

    added user online update

---
+++ client/src/components/Online.jsx
@@ -0,0 +1,22 @@
+// import React, { Component } from 'react';
+
+// class Online extends Component {
+//   constructor(props) {
+//     super(props);
+
+//   }
+
+//   render () {
+//     return (
+//       <div>
+//         <script> var socket = io()
+//           socket.on('broadcast', function(data) {
+//             document.body.innerHTML=data
+//           })
+//         </script>
+//       </div>
+//     )
+//   }
+// }
+
+// export default Online;
